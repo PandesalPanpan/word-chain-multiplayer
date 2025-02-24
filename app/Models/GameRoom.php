41984516373e99e019544f7af4d81a36aaf4fe4e
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2025-02-24 14:36:02 +0800

    fix: send an event only when necessary (used a truth table to code this)

--- app/Models/GameRoom.php
+++ app/Models/GameRoom.php
@@ -25,13 +25,42 @@ class GameRoom extends Model
     protected static function booted(): void
     {
         static::created(queueable(function ($gameRoom) {
-            logger('GameRoom created: '.$gameRoom);
-            event(new GameRoomUpdatedEvent('created', $gameRoom));
+            // If the game room is inactive no need to broadcast
+            if (! $gameRoom->is_active) {
+                return;
+            }
+
+            // Pass the game room with the users count
+            $gameRoom->loadCount('users');
+            event(new GameRoomUpdatedEvent('created', $gameRoom->toArray()));
         }));
 
         static::updated(queueable(function ($gameRoom) {
-            logger('GameRoom updated: '.$gameRoom);
-            event(new GameRoomUpdatedEvent('updated', $gameRoom));
+
+            // Get the old attribute
+            $old_is_active = $gameRoom->getOriginal('is_active');
+
+            // Get the current attribute
+            $current_is_active = $gameRoom->is_active;
+
+            // What are the possibilities
+            // 1. Old: False -> New: False = No need to broadcast
+            if ($old_is_active === false && $current_is_active === false) {
+                return;
+            }
+
+            // 2. Old: True -> New: False = Broadcast without user count and also remove users from the game room
+            if ($old_is_active === true && $current_is_active === false) {
+                $gameRoom->users()->update(['game_room_id' => null]);
+                event(new GameRoomUpdatedEvent('updated', $gameRoom->toArray()));
+
+                return;
+            }
+
+            // 3. Old: False -> New: True = Broadcast
+            // 4. Old: True -> New: True = Broadcast
+            $gameRoom->loadCount('users');
+            event(new GameRoomUpdatedEvent('updated', $gameRoom->toArray()));
         }));
     }
 
@@ -49,6 +78,7 @@ class GameRoom extends Model
     {
         return [
             'is_active' => 'boolean',
+            'in_progress' => 'boolean',
         ];
     }
 }
