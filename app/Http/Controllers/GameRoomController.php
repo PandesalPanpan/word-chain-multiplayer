c1d72e25e800b1ba3d62f1fe6d061ad093f7af97
PandesalPanpan <peterelijahamarticio@iskolarngbayan.pup.edu.ph>
2025-03-04 09:55:54 +0800

    fix: add validation if the room is full

--- app/Http/Controllers/GameRoomController.php
+++ app/Http/Controllers/GameRoomController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\GameRoomClosedEvent;
 use App\Models\GameRoom;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,7 +17,6 @@ class GameRoomController extends Controller
 
     public function getActiveRooms()
     {
-        logger('getActiveRooms called');
 
         $gameRoom = GameRoom::where('is_active', true)
             ->withCount('users')
@@ -28,12 +28,17 @@ class GameRoomController extends Controller
 
     public function show(GameRoom $gameRoom)
     {
+        // Check if the room is active
+        if (! $gameRoom->is_active) {
+            return redirect()->route('game-rooms.index')
+                ->with('error', 'The room is not active');
+        }
+
         // Check if the room is full already
         if ($gameRoom->users->count() >= 2) {
             return redirect()->back()
                 ->with('error', 'The room is full (maximum 2 players allowed)');
         }
-
 
         // Check if the user belongs to a room already
         if (auth()->user()->gameRoom) {
@@ -67,14 +72,18 @@ class GameRoomController extends Controller
             case 2: // User is host
                 // Check if there are other users in the room
                 if ($gameRoom->users->count() > 1) {
-                    $gameRoom->update(['host_id' => $gameRoom->users->first()->id]);
+                    // Make all users leave the room
+                    $gameRoom->users()->update(['game_room_id' => null]);
+                    $gameRoom->update(['is_active' => false]);
+                    // TODO: Trigger an event to notify non host users to be redirected to the index page using alpine
+                    event(new GameRoomClosedEvent($gameRoom));
                 } else {
                     try {
                         DB::transaction(function () use ($gameRoom, $request) {
                             $request->user()->withoutEvents(function () use ($request) {
                                 $request->user()->gameRoom()->dissociate()->save();
                             });
-                            $gameRoom->update(['host_id' => null, 'is_active' => false]);
+                            $gameRoom->update(['is_active' => false]);
                         });
                     } catch (\Throwable $e) {
                         report($e);
@@ -84,7 +93,7 @@ class GameRoomController extends Controller
                 }
                 break;
 
-            case 3: // User in room
+            case 3: // User was in room
                 $request->user()->gameRoom()->dissociate()->save();
                 break;
 
