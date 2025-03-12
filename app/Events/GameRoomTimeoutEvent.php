55c682893f2e2db488fa42deb31d7291154be869
PandesalPanpan <petermarticio@gmail.com>
2025-03-12 17:48:00 +0800

    feat: always remove words used after timeout

--- app/Events/GameRoomTimeoutEvent.php
+++ app/Events/GameRoomTimeoutEvent.php
@@ -14,7 +14,11 @@ class GameRoomTimeoutEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public function __construct(public GameRoom $gameRoom, public User $winner) {}
+    public function __construct(public GameRoom $gameRoom, public User $winner)
+    {
+        // Delete the word moves
+        $gameRoom->wordMoves()->delete();
+    }
 
     public function broadcastOn(): array
     {
